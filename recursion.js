ad22dceda14e1295b6454ff00ea1387078aa59ec
Michel Maia <dimermichel@gmail.com>
2020-05-22 22:52:11 -0400

    Add more examples to the study of Recursion

--- recursion.js
+++ recursion.js
@@ -1,6 +1,10 @@
-// Recursion Pattern
+// Recursion Pattern Study
 
 // Calculate Power similar to Math.pow
+//Write a function called power which accepts a base and an exponent. 
+//The function should return the power of the base to the exponent. 
+//This function should mimic the functionality of Math.pow() - 
+//do not worry about negative bases and exponents.
 function power(base, exp){
     if (exp === 0) return 1;
     return base * power(base, exp - 1);
@@ -10,6 +14,8 @@ console.log(power(2,6))
 
 
 // Calculate the product of an Array
+//Write a function called productOfArray which takes in an array of numbers 
+//and returns the product of them all.
 function productOfArray(arr){
     if (arr.length === 0) return 1;
     return arr[0] * productOfArray(arr.slice(1));
@@ -18,3 +24,50 @@ function productOfArray(arr){
 // productOfArray([1,2,3]) // 6
 // productOfArray([1,2,3,10]) // 60
 console.log(productOfArray([1,2,3,20]))
+
+//FACTORIAL SOLUTION
+//Write a function factorial which accepts a number and returns the factorial of that number. 
+//A factorial is the product of an integer and all the integers below it; 
+//e.g., factorial four (4!) is equal to 24, because 4 * 3 * 2 * 1 equals 24. 
+//Factorial zero (0!) is always 1.
+function factorial(x){
+   if (x < 0 ) return 0;
+   if (x <= 1 ) return 1;
+   return x * factorial(x-1);
+}
+
+//RECURSIVE RANGE SOLUTION
+//Write a function called recursiveRange which accepts a number and adds up all the numbers 
+//from 0 to the number passed to the function
+function recursiveRange(x){
+   if (x === 0 ) return 0;
+   return x + recursiveRange(x-1);
+}
+
+//FIBONACCI SOLUTION
+// Write a recursive function which accepts a number and returns the nth number in the Fibonacci sequence. 
+//Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... 
+//which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
+
+function fib(n){
+    if (n <= 2) return 1;
+    return fib(n-1) + fib(n-2);
+}
+
+//Solution without recursion
+function fibonacciMultiplePointers(num){
+    let arr = [1,1];
+    let pointer1 = 0;
+    let pointer2 = 1;
+    while (arr.length !== num) {
+        arr.push(arr[pointer1] + arr[pointer2]);
+        pointer1++;
+        pointer2++;
+    }
+    return arr[pointer2];
+}
+
+// fib(4) // 3
+// fib(10) // 55
+// fib(28) // 317811
+// fib(35) // 9227465
