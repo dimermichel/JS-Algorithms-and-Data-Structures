e2d59d5299dbb77a2319fcb4783c30fa3f46edb1
Michel Maia <dimermichel@gmail.com>
2020-05-22 22:58:16 -0400

    Add Linear Search

---
+++ linearSearch.js
@@ -0,0 +1,10 @@
+// Linear Search Algorithm - similar to indexOf()
+// O(n)
+function linearSearch(arr, val){
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] === val) return i;
+    }
+    return -1;
+}
+
+linearSearch([34,51,1,2,3,45,56,687], 100)
